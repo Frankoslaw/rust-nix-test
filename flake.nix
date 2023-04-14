0a37d7ff5c24a87d9bbae37b1b01b4d6260755cc
Franciszek Łopuszański <franopusz2006@gmail.com>
2023-04-14 22:12:49 +0200

    Add flake.lock file

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk/master";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk, ... }:
