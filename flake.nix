ee00043f080dfd2303c46a09d2fd25118bc7c1de
Franciszek Łopuszański <franopusz2006@gmail.com>
2023-04-14 13:48:46 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk/master";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        naersk-lib = pkgs.callPackage naersk { };
+        libPath = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+      in
+      with pkgs;
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          doCheck = true;
+          pname = "rust-nix-test";
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = with pkgs; [
+            xorg.libxcb
+          ];
+          postInstall = ''
+            wrapProgram "$out/bin/rust-nix-test" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
+        };
+
+
+        defaultApp = flake-utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
+        };
+
+        devShells.default = mkShell {
+          buildInputs = [
+            cargo
+            cargo-insta
+            pre-commit
+            rust-analyzer
+            rustPackages.clippy
+            rustc
+            rustfmt
+            tokei
+            pkg-config
+            rust-bin.beta.latest.default
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = libPath;
+        };
+      }
+    );
+}
